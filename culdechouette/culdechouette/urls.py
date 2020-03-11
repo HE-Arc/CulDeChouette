45aaba0de229d4ff8fb3c99aa3ce174b582be18a
ninojeannet <nino.jeannet@he-arc.ch>
2020-03-11 17:10:10 +0100

    base template of profile page

--- culdechouette/culdechouette/urls.py
+++ culdechouette/culdechouette/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('register/',user_views.Register.as_view(), name="register"),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('profile/',user_views.ProfileView.as_view(), name="profile"),
     path('',include('main.urls')),
 ]
