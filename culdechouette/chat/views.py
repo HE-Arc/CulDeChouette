628140504ddb79431b537ac74afe1af5dca7c99f
ninojeannet <nino.jeannet@he-arc.ch>
2020-03-31 10:54:45 +0200

    show user name in chat

--- culdechouette/chat/views.py
+++ culdechouette/chat/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.core import serializers
+import json
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
+    user = serializers.serialize('json',[request.user,],fields=('id','username'))
+    struct = json.loads(user)
+    user = json.dumps(struct[0])
+    #user = request.user
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,'user':user
     })
