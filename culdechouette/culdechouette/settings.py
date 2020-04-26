b32ab4a26242d82bcf1acf8f69eeb7c06dd83f1e
John Leuba <john.leuba@he-arc.ch>
2020-04-26 17:44:55 +0200

    Fix static and add capistrano files

--- culdechouette/culdechouette/settings.py
+++ culdechouette/culdechouette/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
