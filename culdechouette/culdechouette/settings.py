0c81f2f9d4d711c2d5e8a5e3894dad7416c761b2
John Leuba <john.leuba@he-arc.ch>
2020-04-28 15:15:11 +0200

    Fix something if god is watching

--- culdechouette/culdechouette/settings.py
+++ culdechouette/culdechouette/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = '/var/www/public/static'
+STATIC_ROOT = '/var/www/static'
 
 STATIC_URL = '/static/'
 
