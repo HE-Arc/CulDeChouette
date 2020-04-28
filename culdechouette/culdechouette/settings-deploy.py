1e06463053277a3e689b5768d849274c7e5faedc
John Leuba <john.leuba@he-arc.ch>
2020-04-28 17:28:16 +0200

    Fix settings files to bu polyvalent

--- culdechouette/culdechouette/settings-deploy.py
+++ culdechouette/culdechouette/settings-deploy.py
@@ -3,4 +3,6 @@ from .settings import *
 # Override DEBUG
 DEBUG = False
 
-ALLOWED_HOSTS += ["culdechouette.srvz-webapp.he-arc.ch"]    
+ALLOWED_HOSTS += ["culdechouette.srvz-webapp.he-arc.ch"]   
+
+STATIC_ROOT = '/var/www/static'
